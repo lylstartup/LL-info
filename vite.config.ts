3fd484a501d69c90716b9f46cf03d1aeef92ac17
lylstartup <lyl.startup@gmail.com>
2026-01-20 21:43:21 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/LL-info/",
+  base: "/LL-INFO/",
   server: {
     host: "::",
     port: 8080,
